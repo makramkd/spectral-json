3ab51ed6849a68846133343e859d826ea5f8a530
Makram Kamaleddine <makram@500px.com>
2017-03-26 18:36:01 -0400

    First commit: `bundle gem spectral-json`

---
+++ lib/spectral/json.rb
@@ -0,0 +1,7 @@
+require "spectral/json/version"
+
+module Spectral
+  module Json
+    # Your code goes here...
+  end
+end
---
+++ lib/spectral/json/version.rb
@@ -0,0 +1,5 @@
+module Spectral
+  module Json
+    VERSION = "0.1.0"
+  end
+end
